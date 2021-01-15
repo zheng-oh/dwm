e801825266491d5595663af73872d746069845d8
zheng-oh <894389673@qq.com>
2021-01-16 02:47:45 +0800

    add rotate

--- config.h
+++ config.h
@@ -93,6 +93,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Right,  spawn,          {.v = wallpaper } },
 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
